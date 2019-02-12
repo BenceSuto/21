af7f008282726c2cb3c2ab804145c93674b6454d
csepdo <arodzeta@gmail.com>
2019-02-12 13:52:57 +0100

    added more pseudo code

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,42 @@
 public class Game {
 	public static void main (String[] args) {
+		public enum deckOfCards = 
 		Player player1 = new Player
 		Player player2 = new Player
 
 		public String nextCard() {
+			/*
+			get random card from remaining deck
+			*/
 		}
 
 		public int countScore(String player) {
+			/*
+			get value of cards in hand
+			add values of new card
+			calculate score
+			*/
 		}
 
 		public String checkWinner() {
+			/*
+			compare scores			
+			if active player is winner false --> continue
+			else return winner + message			
+			*/
+		}
+
+		public void game(player1, player2) {
+			/* 
+			player1 moves --> active player
+			get move
+			count score
+			check winner
+			player2 moves --> active player
+			get move
+			count score
+			check winner
+			*/		
 		}
 	}
 }
