210cf7d18430ec78c5edd2a4ea381ccb4a9dbb73
csepdo <arodzeta@gmail.com>
2019-02-14 10:10:27 +0100

    added comments

--- src/com/codecool/Blackjack/Main.java
+++ src/com/codecool/Blackjack/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
+        //game starts here
         Game game = new Game();
         System.out.println("Welcome to BlackJack!");
         game.main();
