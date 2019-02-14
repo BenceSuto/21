ccd9979c962faf15d83d51a13fe64cec67e94354
Sütő Bence 'Owen <ssben636@gmail.com>
2019-02-14 10:51:16 +0100

    termlib moved and upgraded

--- src/com/codecool/Blackjack/Game.java
+++ src/com/codecool/Blackjack/Game.java
@@ -11,16 +11,23 @@ public class Game {
         int playerBet = 0;
         String player;
         Scanner playerMove = new Scanner(System.in);
+        //terminal.moveTo(10, 20); --> before "enter your name"
         System.out.println("Enter your name: ");
+        //terminal.moveTo(11, 20); --> after "enter your name"
         player = input.nextLine();
+        //terminal.clearScreen(); --> after player = input.nextLine()
         Deck deck = new Deck();
 
         //game continues while player has money
         while (playerMoney >= 0) {
             //player places a bet
+            //terminal.moveTo(10, 20); --> after while (playerMoney > 0)
             System.out.println("You have " + playerMoney + " coins.");
+            //terminal.moveTo(11, 20); --> before "How much money would you like to bet."
             System.out.println("How much money would you like to bet?");
+            //terminal.moveTo(12, 20); --> after "How much money would you like to bet."
             playerBet = playerMove.nextInt();
+            //terminal.clearScreen(); --> after playerBet = playerMove.nextInt()
 
             if (playerBet <= playerMoney) {
                 playerMoney = playerMoney - playerBet;
@@ -40,7 +47,9 @@ public class Game {
                     dealerScore += dealerCards[i].value;
                 }
                 //print out cards
+                //terminal.moveTo(60, 20); --> before System.out.print("Player: " + playerScore); + also change println to print
                 System.out.println("Player: " + playerCards[0] + ", " + playerCards[1]);
+                //terminal.moveTo(60, 170); --> before System.out.print("Dealer: " + dealerScore) + also change println to print
                 System.out.println("Dealer: " + dealerCards[0] + ", [hidden]");
                 //print out scores
                 System.out.println("Player: " + playerScore);
@@ -89,12 +98,14 @@ public class Game {
                 }
 
             } else {
+                //terminal.moveTo(10, 20); --> before "You can't bet more than you have."
                 System.out.println("You can't bet more than you have.");
                 continue;
             }
 
         }
         //player has no more money
+        //terminal.moveTo(10, 20); --> before "Sorry, you have no more money."
         System.out.println("Sorry, you have no more money.");
         playerMove.close();
         //end of game
